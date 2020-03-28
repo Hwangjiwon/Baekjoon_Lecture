0d0d233b16ba792b2500aa7e3a9ad4f699e3ac84
Jiwon <googims330@gmail.com>
2020-03-29 01:26:13 +0900

    yet

--- prac_2583/Main.java
+++ prac_2583/Main.java
@@ -19,20 +19,20 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
 
-		M = Integer.parseInt(input[0]);
-		N = Integer.parseInt(input[1]);
+		M = Integer.parseInt(input[0]); // x
+		N = Integer.parseInt(input[1]); // y
 		K = Integer.parseInt(input[2]);
 
-		map = new int[M][N];
-		vistied = new boolean[M][N];
+		map = new int[N][M];
+		vistied = new boolean[N][M];
 		for (int i = 0; i < K; i++) {
 			input = br.readLine().split(" ");
 
-			int x1 = Integer.parseInt(input[0]);
-			int y1 = Integer.parseInt(input[1]);
+			int y1 = Integer.parseInt(input[0]);
+			int x1 = Integer.parseInt(input[1]);
 
-			int x2 = Integer.parseInt(input[2]);
-			int y2 = Integer.parseInt(input[3]);
+			int y2 = Integer.parseInt(input[2]);
+			int x2 = Integer.parseInt(input[3]);
 
 			for (int y = y1; y < y2; y++) {
 				for (int x = x1; x < x2; x++) {
@@ -41,13 +41,27 @@ public class Main {
 			}
 		}
 
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				width = 0;
+				if (map[i][j] == 0) {
+					cnt++;
+					bfs(i, j);
+				}
+			}
+		}
+
 		print(map);
 		br.close();
 	}
 
+	static void bfs(int y, int x) {
+		
+	}
+
 	static void print(int[][] map) {
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
 				System.out.print(map[i][j]);
 			}
 			System.out.println();
