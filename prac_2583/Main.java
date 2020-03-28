3991e26e61644ccfea46b283653f31a1b1b3ca16
Jiwon <googims330@gmail.com>
2020-03-28 23:54:25 +0900

    yet

--- prac_2583/Main.java
+++ prac_2583/Main.java
@@ -20,7 +20,34 @@ public class Main {
 		N = Integer.parseInt(input[1]);
 		K = Integer.parseInt(input[2]);
 
+		map = new int[M][N];
+		vistied = new boolean[M][N];
+		for (int i = 0; i < K; i++) {
+			input = br.readLine().split(" ");
+
+			int x1 = Integer.parseInt(input[0]);
+			int y1 = Integer.parseInt(input[1]);
+
+			int x2 = Integer.parseInt(input[2]);
+			int y2 = Integer.parseInt(input[3]);
+
+			for (int y = y1; y < y2; y++) {
+				for (int x = x1; x < x2; x++) {
+					map[y][x] = 1;
+				}
+			}
+		}
+
+		print(map);
 		br.close();
 	}
 
+	static void print(int[][] map) {
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
+		}
+	}
 }
