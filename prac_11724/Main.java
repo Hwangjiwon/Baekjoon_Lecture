bac8f924721d6e5dfec0773a2e23d856b9285109
Jiwon <googims330@gmail.com>
2020-04-15 23:53:45 +0900

    Update Main.java

--- prac_11724/Main.java
+++ prac_11724/Main.java
@@ -7,13 +7,16 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
+	static int N, M;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] input = br.readLine().split(" ");
 
-		
+		N = Integer.parseInt(input[0]);
+		M = Integer.parseInt(input[1]);
 		bw.write("");
 		bw.close();
 		br.close();
