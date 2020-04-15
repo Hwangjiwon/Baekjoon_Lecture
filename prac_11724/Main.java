192238211e214da9d71ba7434e2022b1b9ce4c38
Jiwon <googims330@gmail.com>
2020-04-16 00:27:41 +0900

    Update Main.java

--- prac_11724/Main.java
+++ prac_11724/Main.java
@@ -5,9 +5,13 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 public class Main {
 	static int N, M;
+	static ArrayList<ArrayList<Integer>> graph;
+	static boolean[] visited;
+	static int cnt;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -17,9 +21,44 @@ public class Main {
 
 		N = Integer.parseInt(input[0]);
 		M = Integer.parseInt(input[1]);
-		bw.write("");
+
+		graph = new ArrayList<ArrayList<Integer>>();
+		for (int i = 0; i < M; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+
+		visited = new boolean[N + 1];
+
+		for (int i = 0; i < M; i++) {
+			input = br.readLine().split(" ");
+			int u = Integer.parseInt(input[0]);
+			int v = Integer.parseInt(input[1]);
+
+			graph.get(u).add(v);
+			graph.get(v).add(u);
+		}
+
+		cnt = 0;
+		for (int i = 0; i < N; i++) {
+			if (!visited[i]) {
+				dfs(i);
+				cnt++;
+			}
+		}
+
+		bw.write(cnt);
 		bw.close();
 		br.close();
 	}
 
+	public static void dfs(int v) {
+		visited[v] = true;
+
+		for (int u = 0; u < graph.get(v).size(); u++) {
+
+			if (!visited[graph.get(v).get(u)]) {
+				dfs(u);
+			}
+		}
+	}
 }
