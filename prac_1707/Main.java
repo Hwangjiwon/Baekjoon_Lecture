5c749b7b0d1d99bea3bcd47b2c403afb2d3697ed
Jiwon <googims330@gmail.com>
2020-04-19 00:09:15 +0900

    Update Main.java

--- prac_1707/Main.java
+++ prac_1707/Main.java
@@ -3,6 +3,7 @@ package prac_1707;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
 	static int K, V, E;
@@ -18,11 +19,21 @@ public class Main {
 			V = Integer.parseInt(input[0]);
 			E = Integer.parseInt(input[1]);
 
+			// 정점만큼 인접리스트 생성
+			ArrayList<Integer>[] graph = (ArrayList<Integer>[]) new ArrayList[V + 1];
+			for (int j = 1; j <= V; j++) {
+				graph[j] = new ArrayList<>();
+			}
+			
+			// 양방향 연결
 			for (int j = 0; j < E; j++) {
 				input = br.readLine().split(" ");
-				
-				
+				graph[Integer.parseInt(input[0])].add(Integer.parseInt(input[1]));
+				graph[Integer.parseInt(input[1])].add(Integer.parseInt(input[0]));
 			}
+			
+			
+			
 		}
 
 		br.close();
