1727359665dd9f56b0ab35faee129940f933f13f
Jiwon <googims330@gmail.com>
2020-04-19 22:19:45 +0900

    이분그래프, BFS
    연결된 정점마다 color다르게 해서 저장후, 검사하기

--- prac_1707/Main.java
+++ prac_1707/Main.java
@@ -4,15 +4,20 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-	static int K, V, E;
+	static int V, E;
+	static ArrayList<ArrayList<Integer>> graph;
+	static int[] color;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		String[] input = br.readLine().split(" ");
-		K = Integer.parseInt(input[0]);
+		int K = Integer.parseInt(input[0]);
 
 		for (int i = 0; i < K; i++) {
 			input = br.readLine().split(" ");
@@ -20,23 +25,63 @@ public class Main {
 			E = Integer.parseInt(input[1]);
 
 			// 정점만큼 인접리스트 생성
-			ArrayList<Integer>[] graph = (ArrayList<Integer>[]) new ArrayList[V + 1];
-			for (int j = 1; j <= V; j++) {
-				graph[j] = new ArrayList<>();
+			graph = new ArrayList<>();
+			for (int j = 0; j < V; j++) {
+				graph.add(new ArrayList<>());
 			}
-			
+
 			// 양방향 연결
 			for (int j = 0; j < E; j++) {
 				input = br.readLine().split(" ");
-				graph[Integer.parseInt(input[0])].add(Integer.parseInt(input[1]));
-				graph[Integer.parseInt(input[1])].add(Integer.parseInt(input[0]));
+				graph.get(Integer.parseInt(input[0]) - 1).add(Integer.parseInt(input[1]) - 1);
+				graph.get(Integer.parseInt(input[1]) - 1).add(Integer.parseInt(input[0]) - 1);
 			}
-			
-			
-			
-		}
 
+			color = new int[V];
+			for (int j = 0; j < V; j++) {
+				if (color[j] == 0) {
+					color[j] = 1;
+					bfs(j);
+				}
+			}
+
+			// check
+			System.out.println(check());
+		}
 		br.close();
+	}
+
+	public static void bfs(int start) {
+		Queue<Integer> q = new LinkedList<>();
+		q.add(start);
+
+		while (!q.isEmpty()) {
+			int idx = q.poll();
+
+			for (int i = 0; i < graph.get(idx).size(); i++) {
+				int nx = graph.get(idx).get(i);
+
+				if (color[nx] == 0) {
+					if (color[idx] == 1)
+						color[nx] = -1;
+					else if (color[idx] == -1)
+						color[nx] = 1;
+
+					q.add(nx);
+				}
+			}
+
+		}
+	}
+
+	public static String check() {
+		for (int i = 0; i < V; i++) {
+			for (int j = 0; j < graph.get(i).size(); j++) {
+				if (color[i] == color[graph.get(i).get(j)])
+					return "NO";
+			}
+		}
+		return "YES";
 	}
 
 }
