79663cb103824af17fd7032d141d88fca0b693a5
Jiwon <googims330@gmail.com>
2020-04-24 18:03:20 +0900

    Add files via upload

--- prac_2468/Main.java
+++ prac_2468/Main.java
@@ -4,6 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/* 안전 영역
+ * 
+ * 행과 열 N(2~100)
+ * 높이는 1~100
+ * 
+ * 물에 잠기지 않는 최대 개수
+ * 1만큼 잠길때 안전영역 개수
+ * 2만큼 잠길때 안전영역 개수
+ * ... 
+ * 최대 높이-1만큼 잠길때 안전영역 개수
+ */
 public class Main {
 	static int N;
 	static int[][] map;
@@ -15,26 +26,19 @@ public class Main {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] input = br.readLine().split(" ");
-		int highest = 0;
 
 		N = Integer.parseInt(input[0]);
 		map = new int[N][N];
+		visited = new boolean[N][N];
+
 		for (int i = 0; i < N; i++) {
 			input = br.readLine().split(" ");
 			for (int j = 0; j < N; j++) {
 				map[i][j] = Integer.parseInt(input[j]);
-				if (highest < map[i][j])
-					highest = map[i][j];
 			}
 		}
 
 		br.close();
 	}
 
-	public static void dfs() {
-		
-		for (int i = 0; i < N; i++) {
-			
-		}
-	}
 }
