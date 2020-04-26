db635344e70561bb0468bb4376258611d1b43ff1
Jiwon <googims330@gmail.com>
2020-04-26 23:38:46 +0900

    Create Main.java

---
+++ prac_14226/Main.java
@@ -0,0 +1,19 @@
+package prac_14226;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int S;
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		S = Integer.parseInt(br.readLine());
+		
+		
+		br.close();
+	}
+
+}
