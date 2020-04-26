67b826d5fede259e2fcc514b117d3ffddf4eb8d0
Jiwon <googims330@gmail.com>
2020-04-26 23:48:48 +0900

    Update Main.java

--- prac_14226/Main.java
+++ prac_14226/Main.java
@@ -1,5 +1,15 @@
 package prac_14226;
 
+/* 이모티콘
+ * 
+ * S개의 이모티콘 만들기
+ * 복사하기 --> 이전내용 덮어쓰기
+ * 붙여넣기 --> 클립보드의 모든 이모티콘 붙여넣기
+ * 삭제하기 --> 이모티콘 중 하나 삭제
+ * 
+ * 현재 1개의 이모티콘 있는 상태
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
