783072bc92386668fdeee91f227e2c5410f6052b
Jiwon <googims330@gmail.com>
2020-05-03 01:09:53 +0900

    벽부수기, bfs, 3차원 visited로 drill확인

--- prac_2206/Main.java
+++ prac_2206/Main.java
@@ -33,10 +33,11 @@ class Dot {
 public class Main {
 	static int N, M;
 	static int[][] map;
-	static boolean[][] visited;
+	static boolean[][][] visited;
 	static int[] dx = { 0, 0, -1, 1 };
 	static int[] dy = { -1, 1, 0, 0 };
 	static int min = Integer.MAX_VALUE;
+	static int drill;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -47,13 +48,13 @@ public class Main {
 		M = Integer.parseInt(input[1]);
 
 		map = new int[N + 1][M + 1];
-		visited = new boolean[N + 1][M + 1];
+		visited = new boolean[N + 1][M + 1][2];
 
 		for (int i = 1; i <= N; i++) {
 			input = br.readLine().split("");
 
 			for (int j = 1; j <= M; j++) {
-				map[i][j] = Integer.parseInt(input[j]);
+				map[i][j] = Integer.parseInt(input[j - 1]);
 			}
 		}
 
@@ -64,6 +65,43 @@ public class Main {
 
 	public static void bfs() {
 		Queue<Dot> q = new LinkedList<>();
-		q.add(new Dot(1, 1));
+		q.add(new Dot(1, 1, 1, 0));
+
+		while (!q.isEmpty()) {
+			Dot d = q.poll();
+			int px = d.x;
+			int py = d.y;
+			int pc = d.cnt;
+			int pd = d.drill;
+
+			if (px == M && py == N) {
+				min = Math.min(min, pc);
+				return;
+			}
+			for (int i = 0; i < 4; i++) {
+				int nx = px + dx[i];
+				int ny = py + dy[i];
+
+				if (nx <= 0 || ny <= 0 || nx > M || ny > N)
+					continue;
+
+				if (pd == 1) { // 벽 부순적 있는 경우
+					if (!visited[ny][nx][pd] && map[ny][nx] == 0) {
+						visited[ny][nx][pd] = true;
+						q.add(new Dot(ny, nx, pc + 1, pd));
+					}
+				} else { // 벽 부순적 없는 경우
+					if (!visited[ny][nx][pd]) {
+						visited[ny][nx][pd] = true;
+						
+						if (map[ny][nx] == 0) { // 벽 아닌 경우
+							q.add(new Dot(ny, nx, pc + 1, pd));
+						} else if (map[ny][nx] == 1) { // 벽인 경우
+							q.add(new Dot(ny, nx, pc + 1, pd + 1));
+						}
+					}
+				}
+			}
+		}
 	}
 }
