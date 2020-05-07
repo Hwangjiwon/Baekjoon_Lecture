1a78b009a11e0c93669ef9834a79d9de671dceea
Jiwon <googims330@gmail.com>
2020-05-07 23:38:12 +0900

    Create Main.java

---
+++ prac_18290/Main.java
@@ -0,0 +1,20 @@
+package prac_18290;
+
+/*
+ * NM과 K(1)
+ * 
+ * NM의 격자판에서 K개 선택
+ * 선택한 수를 모두 더한 값의 최대값 구하기
+ * 선택한 수가 인접해서는 안됨
+ * 
+ */
+
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
